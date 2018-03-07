05be2ec8535ba3182bdc1908afece563cad46446
Tagman <chartig@hs-mittweida.de>
2018-03-07 18:02:25 +0100

    add deploy script and target
    now all generated results (unit, coverage, clang) and the compiled binaries of the tests plus the socketlib itself will no be copied to an extra folder. Rationale behind this is, to ease the upload to an artifactory like github releases

---
+++ scripts/deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [[ $# -ne 4 ]]
+then
+    echo "Usage: -s path/to/search/start/ -d /path/to/deploy/"
+else
+    PWD=$(pwd)
+    EXECUTE_DIR=$(dirname $0)
+    echo "Dirname: ${PWD}"
+    echo "PWD: ${SCRIPT_DIR}"
+    SEARCH=""
+    DEPLOY=""
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -s|--searchPath)
+        SEARCH="$2"
+        shift #shift past argument
+        shift #shift past value
+        ;;
+        -d|--deployPath)
+        DEPLOY="$2"
+        shift
+        shift
+        ;;
+    esac
+    done
+    echo ${SEARCH}
+    echo ${DEPLOY}
+    mkdir -p "${DEPLOY}"/binaries
+    mkdir -p "${DEPLOY}"/results
+
+    find "${SEARCH}" -iname '*socket.a' -type f -exec cp {} "${DEPLOY}"/binaries \;
+    find "${SEARCH}" -iname 'unit_*' -executable -type f -exec cp {} "${DEPLOY}"/binaries \;
+
+    find "${SEARCH}" -iname 'result_*.xml' -type f -exec cp {} "${DEPLOY}"/results \;
+    find "${SEARCH}" -iname 'result_*' ! -iname '*.dir' -type d -exec cp -r {} "${DEPLOY}"/results/ \;
+
+fi
