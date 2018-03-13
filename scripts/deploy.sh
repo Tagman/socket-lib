b5e545c48d490099fcf69dd40d1a892ed6c1f2d8
Tagman <chartig@hs-mittweida.de>
2018-03-13 17:41:02 +0100

    add deploy to github

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,12 +5,24 @@ if [ $# -ne 1 ]; then
 else
     SCRIPT_DIR=$(dirname $0)
     ARTIFACT_DIR=${SCRIPT_DIR}/../artifacts
+    PROJECT_ROOT=${SCRIPT_DIR}/..
     cp ${SCRIPT_DIR}/index.html ${ARTIFACT_DIR}/results/index.html
 
-    git add ${ARTIFACT_DIR}/results/
+    cd ${PROJECT_ROOT}
+    git clone https://$GITHUB_TOKEN:x-oauth-basic@github.com/tagman/socket-lib.git tmp
+    cd tmp
+    git fetch
+    git checkout gh-pages
+    git pull
+    git rm -r *
+    echo ${PWD}
+    cp -Rf ${PWD}/../artifacts/results/* .
+
+    git add .
     git commit -m $1
+    git push
 
-    cd ${SCRIPT_DIR}/..
+    #cd ${SCRIPT_DIR}/..
 
-    git subtree push --prefix=artifacts/results origin gh-pages
+   # git subtree push --prefix=artifacts/results origin gh-pages
 fi
