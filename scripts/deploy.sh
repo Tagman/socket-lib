4552326390f36a8a07580f6f553f31c69d5c7525
Tagman <chartig@hs-mittweida.de>
2018-03-19 17:27:53 +0100

    trying to fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,20 @@ if [ $# -ne 1 ]; then
     echo "Usage: deploy.sh <Commit Message>"
 else
     SCRIPT_DIR=$(dirname $0)
+    echo "Script_dir: $SCRIPT_DIR"
     ARTIFACT_DIR=${SCRIPT_DIR}/../artifacts
     PROJECT_ROOT=${SCRIPT_DIR}/..
+
+    #controlling
+    echo "Script_dir: $SCRIPT_DIR"
+    echo "Artifact_dir: $ARTIFACT_DIR"
+    echo "PROJECT_ROOT $PROJECT_ROOT"
     cp ${SCRIPT_DIR}/index.html ${ARTIFACT_DIR}/results/index.html
 
     cd ${PROJECT_ROOT}
-    git clone https://$GITHUB_TOKEN:x-oauth-basic@github.com/tagman/socket-lib.git tmp &2>/dev/null
-    cd tmp
+    #mkdir tmp && cd tmp
+    git clone https://$GITHUB_TOKEN:x-oauth-basic@github.com/tagman/socket-lib.git tmp > /dev/null 2>&1
+    cd tmp/
     git fetch
     git checkout gh-pages
     git pull
@@ -20,7 +27,7 @@ else
 
     git add .
     git commit -m $1
-    git push &2>/dev/null
+    git push > /dev/null 2>&1
 
     #cd ${SCRIPT_DIR}/..
 
